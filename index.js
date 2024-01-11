d5a9f509e2616d84c615e71394f68e6ef140e66b
tobywan1 <124063186+tobywan1@users.noreply.github.com>
2024-01-11 16:12:59 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css'; // Import your global styles if any
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
